b54449ab0592e1837bf0d56a1a72c372227f1db8
Jacob Schaefer <jacobschaefer12@gmail.com>
2018-02-13 10:10:59 -0800

    Made mobile menu interactive with cool transition effect

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,5 @@
+// Imports in the 'export' from MobileMenu.js
+import MobileMenu from './modules/MobileMenu'; 
+
+// Designates the variable 'mobileMenu' is a new instance of the class 'MobileMenu'
+var mobileMenu = new MobileMenu();
