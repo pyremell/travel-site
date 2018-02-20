9d8030715981d4fd772a60fbd71287a7ec60cc80
Jacob Schaefer <jacobschaefer12@gmail.com>
2018-02-20 15:04:56 -0800

    Completed the reveal on scroll behavior

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,9 @@
 // Imports in the 'export' from MobileMenu.js
 import MobileMenu from './modules/MobileMenu'; 
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 // Designates the variable 'mobileMenu' is a new instance of the class 'MobileMenu'
 var mobileMenu = new MobileMenu();
+new RevealOnScroll($(".feature-item"), "85%");
+new RevealOnScroll($(".testimonial"), "60%");
