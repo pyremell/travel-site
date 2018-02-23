e4544ad3707f8c1933d5bdc051e2cd2fe81eb004
Jacob Schaefer <jacobschaefer12@gmail.com>
2018-02-23 10:26:11 -0800

    Completed Get in Touch modal

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -3,9 +3,11 @@ import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
 import $ from 'jquery';
 import StickyHeader from './modules/StickyHeader';
+import Modal from './modules/Modal';
 
 // Designates the variable 'mobileMenu' is a new instance of the class 'MobileMenu'
 var mobileMenu = new MobileMenu();
 new RevealOnScroll($(".feature-item"), "85%");
 new RevealOnScroll($(".testimonial"), "60%");
 var stickyHeader = new StickyHeader();
+var modal = new Modal();
